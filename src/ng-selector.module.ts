80d5f357e9d96690c9bb648e6a760ae0bfcb91e7
Yoann Prot <yoann.prot@gmail.com>
2017-12-08 16:00:03 +0100

    :bug: add missing ts config

--- src/ng-selector.module.ts
+++ src/ng-selector.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { NgSelectorComponent } from './ng-selector.component';
 import { NgSelectorValidator } from './ng-selector.validator';
 
 @NgModule({
+    imports: [
+        FormsModule,
+    ],
     declarations: [
         NgSelectorComponent,
         NgSelectorValidator,
